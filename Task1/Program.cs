d9584c193cad905199429ed8e99260a4b09952e0
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-10-18 17:53:32 +0300

    Программа, которая на входе принимает два числа и выдаёт, какое число большее , а какое меньшее.

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая на входе принимает два числа и выдаёт, какое число большее , а какое меньшее.
+
+Console.Write("Введите первое число: ");
+string number1 = Console.ReadLine();
+int a = Convert.ToInt32(number1);
+
+Console.Write("Введите второе число: ");
+string number2 = Console.ReadLine();
+int b = Convert.ToInt32(number2);
+
+if (a > b)
+{
+    Console.WriteLine("Наибольшее число: " + a);
+}
+
+else
+{
+    Console.WriteLine("Наибольшее число: " + b);
+}
+
+
+
