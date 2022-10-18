8c768412dfaa9f40c4676e8c23584655e05b9889
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-10-18 19:12:17 +0300

    Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите первое число: ");
+string number1 = Console.ReadLine();
+int a = Convert.ToInt32(number1);
+
+
+for (int x = 1; x <= a; x++)
+    if (x % 2 == 0)
+
+        Console.WriteLine(x);
+
+
+
+        
+
+
+
