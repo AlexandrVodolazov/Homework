91a1d3bb4619b822ddc595793272be4f487c5b7c
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-10-18 18:44:30 +0300

    Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите первое число: ");
+string number1 = Console.ReadLine();
+int a = Convert.ToInt32(number1);
+
+
+
+
+if (a % 2 == 0)
+{
+    Console.WriteLine(a + " - является четным");
+}
+
+else
+{
+    Console.WriteLine(a + " - не является четным");
+}
+
+
+
